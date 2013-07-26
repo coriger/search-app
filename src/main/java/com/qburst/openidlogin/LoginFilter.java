8f5f7129db1da9ad87cf6c142fb9614887b87730
sunil <sunil@sunil.(none)>
2013-07-26 16:19:43 +0530

    Changes for OpenId based SSO

---
+++ src/main/java/com/qburst/openidlogin/LoginFilter.java
@@ -0,0 +1,114 @@
+package com.qburst.openidlogin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.expressme.openid.OpenIdManager;
+
+public class LoginFilter implements Filter {
+	public static final String OPENID_INDENTITY = "openIdIdentity";
+	public static final String LOGOUT_URL = "logoutURL";
+	Properties props = new Properties();
+	String logoutURL;
+	boolean logoutSessionOnly;
+	boolean isOpen;
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+		InputStream input = null;
+
+		try {
+			input = getClass().getClassLoader().getResourceAsStream(
+					"openid-urls.properties");
+
+			props.load(input);
+
+		} catch (IOException e) {
+			// load failed:
+			e.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+
+		OpenIdManager manager = new OpenIdManager();
+		manager.setRealm(props.getProperty("realm"));
+		manager.setReturnTo(props.getProperty("returnToURL"));
+		logoutURL = props.getProperty("OpenIdLogoutURL");
+		isOpen = "none".equals(props.getProperty("security"));
+		logoutSessionOnly = "session".equals(props.getProperty("logoutType"));
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		
+		if(httpRequest.getRequestURI().matches(".*(css|jpg|png|gif|js)")){
+		    chain.doFilter(request, response);
+		    return;
+		}
+		
+		if (!isOpen) {
+			String action = request.getParameter("action");
+			if (action != null && "logout".equalsIgnoreCase(action)) {
+				HttpSession session = httpRequest.getSession(false);
+				session.removeAttribute(OPENID_INDENTITY);
+				session.invalidate();
+
+				if (logoutSessionOnly) {
+					// Redirect to the login page
+					request.getRequestDispatcher("/WEB-INF/view/login.jsp")
+							.forward(httpRequest, httpResponse);
+				} else {
+					// Redirect to the logout URL
+					httpResponse.sendRedirect(logoutURL);
+				}
+				return;
+			}
+
+			else if (!(httpRequest.getRequestURL().toString().equals(props
+					.getProperty("loginURL")))) {
+				HttpSession session = httpRequest.getSession(false);
+				if (session == null
+						|| session.getAttribute(OPENID_INDENTITY) == null) {
+					httpResponse.sendRedirect(props.getProperty("loginURL")
+							+ "?op=" + props.getProperty("openIdProvider"));
+					return;
+				}
+
+			}
+		}
+		else
+		{
+			request.setAttribute("noAuthentication","Y");
+		}
+		chain.doFilter(request, response);
+
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/view/components/userProfile.jsp
@@ -0,0 +1,29 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<link rel="stylesheet"
+	href="<c:url value="/resources/css/bootstrap-combined.min.css"/>"
+	type="text/css" />
+<link rel="stylesheet"
+	href="<c:url value="/resources/css/bootstrap.css" />" type="text/css" />
+
+<!-- latest jQuery, Boostrap JS and hover dropdown plugin -->
+<script
+	src="<c:url value="/resources/js/twitter-bootstrap-hover-dropdown.min.js"/>"></script>
+<script src="<c:url value="/resources/js/jquery-latest.min.js"/>"></script>
+<script src="<c:url value="/resources/js/bootstrap.min.js"/>"></script>
+		<div class="container">
+			<ul class="nav pull-right">
+				<li class="dropdown"><a href="#" class="dropdown-toggle"
+					data-toggle="dropdown" data-hover="dropdown" data-delay="1000"
+					data-close-others="false"> <c:out
+							value="${sessionScope.openIdIdentity.fullname}" />
+				</a>
+					<ul class="dropdown-menu">
+						<li><a tabindex="-1" href="#">Your Profile</a></li>
+						<li class="divider"></li>
+						<li><a tabindex="-1" href="sessions?action=logout">Logout</a></li>
+					</ul></li>
+			</ul>
+		</div>
+
+<!-- See more at:
+http://www.w3resource.com/twitter-bootstrap/navbar-tutorial.php#sthash.TKj9gbx9.dpuf -->

40d46b7839fbf1fe3311fe0b250c1077e77f6f6d
Cyril Cherian <cyril@mobomo.com>
2013-07-30 16:38:28 +0530

    Visualization fixes.

--- src/main/webapp/resources/js/searchApp.js
+++ src/main/webapp/resources/js/searchApp.js
@@ -34,87 +34,8 @@ searchApp.controller("SearchController", function($scope, $http, $dialog, pdfSea
 				$scope.result_text = "Results of "+keyword;
 			}
 			$scope.$apply();
-			$scope.addVisualization();
 		});
-	};
-	$scope.addVisualization = function(){
-		var nodes = $scope.results;
-		var root = nodes[0];
-		root.fixed = true;
-		var highlightsArr = [];
-		var color = d3.scale.category20();
-		var w = $("#vis").width(), h = $("#vis").height()-5;
-		if($(".svg_vis").length > 0)
-			$(".svg_vis").remove();
-		if(nodes.length <= 0)
-			return;
-		else if(nodes.length == 1)
-			data = nodes;
-		else
-			data = nodes.slice(1);
-		var force = d3.layout.force()
-		    .gravity(0.05)
-		    .charge(function(d, i) { return i ? 0 : -2000; })
-		    .nodes(nodes)
-		    .size([w, h]);
-		force.start();
-		var svg = d3.select("#vis").append("svg:svg")
-		    .attr("width", w)
-		    .attr("height", h)
-		    .attr("class","svg_vis");
-		nodes.forEach(function(d,i){ 
-			highlightsArr.push(d.highlights.length); 
-		});
-		var radiusScale = d3.scale.linear()
-			.domain([d3.min(highlightsArr), d3.max(highlightsArr)])
-			.range([6, 100]);
-		svg.append("svg:rect")
-		    .attr("width", w)
-		    .attr("height", h);
-		var circle = svg.selectAll("circle")
-		    .data(data)
-		    .enter().append("svg:circle")
-		    .attr("r", function(d) {
-		    	d.radius = radiusScale(d.highlights.length);
-		    	return d.radius; 
-		    })
-		    .style("fill", function(d, i) { return color(i) })
-		    .call(force.drag)
-		    .append("svg:title")
-		    .text(function(d){ return d.title; });
-		force.on("tick", function(e) {
-			  var q = d3.geom.quadtree(nodes),
-			      i = 0,
-			      n = nodes.length;
-			  while (++i < n) q.visit(collide(nodes[i]));
-			  svg.selectAll("circle")
-			      .attr("cx", function(d) { return d.x = Math.max(d.radius, Math.min(w - d.radius, d.x))  })
-			      .attr("cy", function(d) { return d.y = Math.max(d.radius, Math.min(h - d.radius, d.y)); });
-		});
-		function collide(node) {
-			var r = node.radius + 16,
-				nx1 = node.x - r,
-				nx2 = node.x + r,
-				ny1 = node.y - r,
-				ny2 = node.y + r;
-			return function(quad, x1, y1, x2, y2) {
-				if (quad.point && (quad.point !== node)) {
-					var x = node.x - quad.point.x,
-					y = node.y - quad.point.y,
-					l = Math.sqrt(x * x + y * y),
-					r = node.radius + quad.point.radius;
-					if (l < r) {
-						l = (l - r) / l * .5;
-						node.x -= x *= l;
-						node.y -= y *= l;
-						quad.point.x += x;
-						quad.point.y += y;
-					}
-				}
-				return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-			};
-		}
-	};
+	};	
 	$scope.show = function(item){
 		angular.extend($scope.opts, {resolve: {item: function(){ return angular.copy(item); }}})
 	    var d = $dialog.dialog($scope.opts);
@@ -129,12 +50,16 @@ searchApp.controller("SearchController", function($scope, $http, $dialog, pdfSea
         	if(file.type == "application/pdf") file.response = "resources/images/success.png"
         	else  file.response = "resources/images/error.png"
         	$scope.files.push(file);
+        	$scope.hasFiles = true;
         });
     });
     $scope.deleteFile = function(index) {
     	$scope.files.splice(index,1)
+    	if($scope.files.length <= 0)
+    		$scope.hasFiles = false;
     };
     $scope.upload = function(){
+    	$scope.isUpload = true;
     	if($scope.files.length > 0){
     		$http({
                 method: 'POST',
@@ -155,12 +80,15 @@ searchApp.controller("SearchController", function($scope, $http, $dialog, pdfSea
                 data: { files: $scope.files }
             }).
             success(function (data, status, headers, config) {
+            	$scope.isUpload = false;
             	$scope.showUploadStatus(data);
             	$scope.files = [];
+            	$scope.hasFiles = false;
                 console.log(data);
                 console.log(status);
             }).
             error(function (data, status, headers, config) {
+            	$scope.isUpload = false;
             	alert("failed!");
                 console.log(data);
                 console.log(status);
@@ -168,7 +96,7 @@ searchApp.controller("SearchController", function($scope, $http, $dialog, pdfSea
     	}
     };
     $scope.showUploadStatus = function(files){
-    	var d = $dialog.dialog({ backdrop: true, keyboard: true, dialogFade: true, resolve: {files: function(){ return angular.copy(files); } }})
+    	var d = $dialog.dialog({ backdrop: true, keyboard: true, backdropClick: true, dialogFade: true, resolve: {files: function(){ return angular.copy(files); } }})
     	d.open('resources/template/upload_status.html', 'UploadController');
     }
 });
@@ -183,4 +111,87 @@ searchApp.controller("UploadController", function($scope, files, dialog) {
 	$scope.close = function(){
 	   dialog.close(undefined);
 	};
+});
+searchApp.directive('drawVisualization', function ($dialog) {
+    return {
+		restrict: 'E',
+	    scope: {
+	      val: '=',
+	    },
+        link: function (scope, element, attrs) {
+        	scope.$watch('val', function (newVal, oldVal) {
+        		if (!newVal || newVal == []) {
+        	          return;
+    	        }
+        		var w = 1210, h = 360, nodes = newVal, highlightsArr = [];
+        		var color = d3.scale.category20();
+        		if($(".svg_vis").length > 0)
+        			$(".svg_vis").remove();
+        		if(nodes.length <= 0)
+        			return;
+        		var force = d3.layout.force()
+        		    .gravity(0.5)
+        		    .charge(function(d, i) { return i ? 0 : -2000; })
+        		    .nodes(nodes)
+        		    .size([w, h]);
+        		force.start();
+        		var svg = d3.select(element[0]).append("svg:svg")
+        		    .attr("width", w)
+        		    .attr("height", h)
+        		    .attr("class","svg_vis");
+        		nodes.forEach(function(d,i){ 
+        			highlightsArr.push(d.highlights.length); 
+        		});
+        		var radiusScale = d3.scale.linear()
+        			.domain([d3.min(highlightsArr), d3.max(highlightsArr)])
+        			.range([6, 100]);
+        		svg.append("svg:rect")
+        		    .attr("width", w)
+        		    .attr("height", h);
+        		var circle = svg.selectAll("circle")
+        		    .data(nodes)
+        		    .enter().append("svg:circle")
+        		    .attr("r", function(d) {
+        		    	d.radius = radiusScale(d.highlights.length);
+        		    	return d.radius; 
+        		    })
+        		    .style("fill", function(d, i) { return color(i) })
+        		    .call(force.drag)
+        		    .append("svg:title")
+        		    .text(function(d){ return d.title; });
+        		force.on("tick", function(e) {
+        			  var q = d3.geom.quadtree(nodes),
+        			      i = 0,
+        			      n = nodes.length;
+        			  while (++i < n) q.visit(collide(nodes[i]));
+        			  svg.selectAll("circle")
+        			      .attr("cx", function(d) { return d.x = Math.max(d.radius, Math.min(w - d.radius, d.x))  })
+        			      .attr("cy", function(d) { return d.y = Math.max(d.radius, Math.min(h - d.radius, d.y)); });
+        		});
+        		function collide(node) {
+        			var r = node.radius + 16,
+        				nx1 = node.x - r,
+        				nx2 = node.x + r,
+        				ny1 = node.y - r,
+        				ny2 = node.y + r;
+        			return function(quad, x1, y1, x2, y2) {
+        				if (quad.point && (quad.point !== node)) {
+        					var x = node.x - quad.point.x,
+        					y = node.y - quad.point.y,
+        					l = Math.sqrt(x * x + y * y),
+        					r = node.radius + quad.point.radius;
+        					if (l < r) {
+        						l = (l - r) / l * .5;
+        						node.x -= x *= l;
+        						node.y -= y *= l;
+        						quad.point.x += x;
+        						quad.point.y += y;
+        					}
+        				}
+        				return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+        			};
+        		}
+        	});
+    	}
+	}
 });
